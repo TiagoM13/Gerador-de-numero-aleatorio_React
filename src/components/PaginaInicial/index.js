ecd0153532f45d463c78443457eff9f7401f29a8
TiagoM13 <thyagomotha2000@gmail.com>
2021-04-19 13:20:22 -0300

    Primeira aplicação React

---
+++ src/components/PaginaInicial/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './style.css'
+
+export default function PaginaInicial() {
+// Variável que recebe o número
+   const [ numeroAleatorio, setNumeroAleatorio ] = useState(0);
+
+// Função de gerar o número aleatório
+    function gerarNumero() {
+        const novoNumero = Math.floor(Math.random() * (100 - 1) + 1);
+        setNumeroAleatorio(novoNumero);
+}
+
+  return (
+      <div className="conteudo-centralizado">
+          <h3>Número Aleatório:</h3>
+          <h1>{ numeroAleatorio }</h1>
+
+          <div className="area-botao">
+                <label>
+                    Click no botão abaixo para gerar um número aleatório:
+                </label>
+
+                <button onClick={ gerarNumero }> 
+                    Gerar número
+                </button>
+          </div>
+      </div>
+  );
+}
+ 
